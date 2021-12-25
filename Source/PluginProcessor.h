45a2985d4a072c2f79a4040756bda2757658c7f6
ErwanGateauMagdeleine <erwan.magdeleine@gmail.com>
2021-12-26 00:30:34 +0100

    compressor band as a separate object

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -13,6 +13,24 @@
 //==============================================================================
 /**
 */
+struct CompressorBand
+{
+    juce::AudioParameterFloat* attack{ nullptr };
+    juce::AudioParameterFloat* release{ nullptr };
+    juce::AudioParameterFloat* threshold{ nullptr };
+    juce::AudioParameterChoice* ratio{ nullptr };
+    juce::AudioParameterBool* bypassed{ nullptr };
+
+    void prepare(const juce::dsp::ProcessSpec& spec);
+    void updateCompressorSettings();
+    void process(juce::AudioBuffer<float>& buffer);
+private:
+    juce::dsp::Compressor<float> compressor;
+};
+
+//==============================================================================
+/**
+*/
 class SimpleMBCompAudioProcessor  : public juce::AudioProcessor
 {
 public:
@@ -60,14 +78,7 @@ public:
 
 private:
 
-    juce::dsp::Compressor<float> compressor;
-
-    juce::AudioParameterFloat* attack{ nullptr };
-    juce::AudioParameterFloat* release{ nullptr };
-    juce::AudioParameterFloat* threshold{ nullptr };
-    juce::AudioParameterChoice* ratio{ nullptr };
-    juce::AudioParameterBool* bypassed{ nullptr };
-
+    CompressorBand compressor;
 
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleMBCompAudioProcessor)
