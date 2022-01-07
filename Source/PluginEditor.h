6deacb8a2d4aad2ce3c732873613a60184f0672d
ErwanGateauMagdeleine <erwan.magdeleine@gmail.com>
2022-01-07 12:09:17 +0100

    Add control buttons to Compressor Band Controls

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -181,7 +181,12 @@ private:
     RatioSlider ratioSlider;
 
     using Attachment = juce::AudioProcessorValueTreeState::SliderAttachment;
-    std::unique_ptr< Attachment> attackSliderAttachment, releaseSliderAttachment, thresholdSliderAttachment, ratioSliderAttachment;
+    std::unique_ptr<Attachment> attackSliderAttachment, releaseSliderAttachment, thresholdSliderAttachment, ratioSliderAttachment;
+
+    juce::ToggleButton bypassButton, soloButton, muteButton, lowBand, midBand, highBand;
+
+    using BtnAttachment = juce::AudioProcessorValueTreeState::ButtonAttachment;
+    std::unique_ptr<BtnAttachment> bypassButtonAttachment, soloButtonAttachment, muteButtonAttachment;
 };
 
 //==============================================================================
