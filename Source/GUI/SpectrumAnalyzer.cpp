b1cb3ed446d95da100a1312e0d553fd810f4f94f
ErwanGateauMagdeleine <erwan.magdeleine@gmail.com>
2022-01-09 16:44:31 +0100

    Make use of MIN and MAX frequency definitions

--- Source/GUI/SpectrumAnalyzer.cpp
+++ Source/GUI/SpectrumAnalyzer.cpp
@@ -63,7 +63,7 @@ std::vector<float> SpectrumAnalyzer::getXs(const std::vector<float>& freqs, floa
 
     for (auto f : freqs)
     {
-        auto normX = juce::mapFromLog10(f, 20.f, 20000.f);
+        auto normX = juce::mapFromLog10(f, MIN_FREQUENCY, MAX_FREQUENCY);
         xs.push_back(left + width * normX);
     }
 
